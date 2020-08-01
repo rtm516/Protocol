54d01b8ce13c30ddfbb62c9eca4c718bbc5612b8
rtm516 <ryantmilner@hotmail.co.uk>
2020-08-01 18:59:14 +0100

    Add start of v9 support

---
+++ bedrock/bedrock-v9/src/main/java/com/nukkitx/protocol/bedrock/v9/serializer/LoginSerializer_v9.java
@@ -0,0 +1,38 @@
+package com.nukkitx.protocol.bedrock.v9.serializer;
+
+import com.nukkitx.network.VarInts;
+import com.nukkitx.protocol.bedrock.BedrockPacketHelper;
+import com.nukkitx.protocol.bedrock.BedrockPacketSerializer;
+import com.nukkitx.protocol.bedrock.packet.LoginPacket;
+import io.netty.buffer.ByteBuf;
+import io.netty.util.AsciiString;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginSerializer_v9 implements BedrockPacketSerializer<LoginPacket> {
+    public static final LoginSerializer_v9 INSTANCE = new LoginSerializer_v9();
+
+
+    @Override
+    public void serialize(ByteBuf buffer, BedrockPacketHelper helper, LoginPacket packet) {
+        buffer.writeInt(packet.getProtocolVersion());
+
+        AsciiString chainData = packet.getChainData();
+        AsciiString skinData = packet.getSkinData();
+
+        VarInts.writeUnsignedInt(buffer, chainData.length() + skinData.length() + 8);
+
+        helper.writeLEAsciiString(buffer, chainData);
+        helper.writeLEAsciiString(buffer, skinData);
+    }
+
+    @Override
+    public void deserialize(ByteBuf buffer, BedrockPacketHelper helper, LoginPacket packet) {
+        packet.setProtocolVersion(buffer.readInt());
+
+        ByteBuf jwt = buffer.readSlice(VarInts.readUnsignedInt(buffer)); // Get the JWT.
+        packet.setChainData(helper.readLEAsciiString(jwt));
+        packet.setSkinData(helper.readLEAsciiString(jwt));
+    }
+}
